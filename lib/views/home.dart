0cde902e8f953348237bf83171f24598d7e24b58
devilghostg <mister.devil6@gmail.com>
2025-09-12 14:16:36 +0400

    header / nav / titlle

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -14,30 +14,179 @@ class _HomeState extends State<Home> {
       extendBodyBehindAppBar: true,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
-        title: Text('VOLKANO'),
+        title: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  SizedBox(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(50),
+                      child: Image(
+                        height: 50,
+                        width: 50,
+                        image: AssetImage('assets/images/profil/profil.webp'),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Welcome Back',
+                            style: TextStyle(
+                              color: Colors.white70,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 14,
+                            ),
+                          ),
+                          Text(
+                            'Boyer Dorian',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                decoration: BoxDecoration(
+                  color: Colors.white70,
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Icon(Icons.notifications, color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
+
       body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
         child: Stack(
           children: [
-            Container(
-              height: 500,
-              width: double.infinity,
-              alignment: Alignment.center,
-              // color: Colors.red,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-                child: Image(
-                  image: AssetImage('assets/images/volcano.webp'),
-                  fit: BoxFit.cover,
+            Padding(
+              padding: const EdgeInsets.only(top: 5, left: 5, right: 5),
+              child: Container(
+                height: 500,
+                width: double.infinity,
+                alignment: Alignment.center,
+                // color: Colors.red,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(45),
+                  child: Image(
+                    image: AssetImage('assets/images/volcano.webp'),
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
             ),
             Positioned(
+              bottom: 30,
+              left: 50,
+              right: 50,
               child: Container(
-                height: 3,
+                height: 250,
+                width: double.infinity,
                 alignment: Alignment.center,
-                color: Colors.yellow,
-                child: Column(),
+                // color: Colors.yellow,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      child: Row(
+                        children: [
+                          SizedBox(
+                            child: Icon(
+                              Icons.maps_ugc_outlined,
+                              color: Colors.white,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(5.0),
+                            child: SizedBox(
+                              child: Text(
+                                'Location',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 17,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      child: Text(
+                        'Arenal Volcano',
+                        style: TextStyle(
+                          fontSize: 45,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      child: Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(3.0),
+                            child: SizedBox(
+                              child: Icon(
+                                Icons.star,
+                                color: Colors.orangeAccent,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(2.0),
+                            child: SizedBox(
+                              child: Text(
+                                '4/5',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18,
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            child: Text(
+                              "(500+Reviews)",
+                              style: TextStyle(
+                                color: Colors.white70,
+                                fontSize: 15,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(child: FormField(builder: ),)
+                  ],
+                ),
               ),
             ),
           ],
